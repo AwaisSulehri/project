3aecc44500648a730797a482eefc456ee722d6a0
AwaisSulehri <awaissulehri24@gmail.com>
2024-08-11 00:28:43 +0500

    Add files via upload

---
+++ Awais_assign2.py
@@ -0,0 +1,99 @@
+#Question: 1
+awais = "How are you?"
+print (awais)
+
+#Question: 2
+variable = "Hello World!"
+print (variable)
+variable = "New message assigned to variable."
+print (variable)
+
+#Question: 3
+person = "Awais"
+print (person + ", would you like to learn some Python today?")
+
+#Question: 4
+name = "Rehman"
+print ("Lowercase: " + name.lower())
+print ("Uppercase: " + name.upper())
+print ("Title: " + name.title())
+
+#Question: 5
+author = "Herman Melville"
+print (author + " once said, " + " “It is better to fail in originality than to succeed in imitation.” ")
+
+#Question: 6
+famous_person = "Ray Bradbury"
+message = "“Life is trying things to see if they work.”"
+print (f"{famous_person} once said, {message}")
+
+#Question: 7
+person_name = "\t \n    Usman   \n \t"
+print (person_name)
+print (person_name.lstrip())
+print (person_name.rstrip())
+print (person_name.strip())
+
+#Question: 8
+x = 5
+y = 10
+z = 15
+print(x+y+z)
+
+#Question: 9
+a = 5
+b = 10
+print(f"{a} and {b}")
+#After swap
+c = a
+a = b
+b = c
+print(f"{a} and {b}")
+
+
+#Question: 10
+favorite_color = "Green"
+print(favorite_color)
+color = "Green"
+print(color)
+
+#Question: 11
+pet_name = "Buddy"
+print(pet_name)
+pet_name = "Max"
+print(pet_name)
+
+#Question: 12
+# sunshine = "Sunshine"
+# print(sunshine)
+# print(sunsine)
+
+#Question: 13
+score = 100
+print(score)
+score = 150
+print(score)
+
+#Question: 14
+city = "Lahore"
+print(city)
+
+#Question: 15
+text = "python programming"
+print(text.title())
+
+#Question: 16
+text1 = "Life is Trying things to see if they Work."
+print(text1.lower())
+
+#Question: 17
+text2 = "Life is trying Things to see if they Work."
+print(text2.upper())
+
+#Question: 18
+temperature = 25
+print(f"The current temperature is {temperature} degrees.")
+
+#Question: 19
+poem = "In the forest where shadows plays, \n Lies a path that winds away. \n Moonlight dances on the stream, \n Fireflies flicker in a dream. \n Stars above put on a show, \n Whisper fill the night below. \n Nature's song, wild and free, \n Calls the heart to mystery."
+print (poem)
